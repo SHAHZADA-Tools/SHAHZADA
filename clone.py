50d58e5a18f07d3c68208312d56376d83a6a60db
SHAHZADA-Tools <140398793+SHAHZADA-Tools@users.noreply.github.com>
2023-07-24 22:44:52 +0500

    Update clone.py

--- clone.py
+++ clone.py
@@ -1,1 +1,12 @@
-import free
+import os
+try:import httpx
+except:os.system('pip install httpx > /dev/null')
+try:import requests
+except:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+os.system('git pull')
+from platform import uname
+bt=uname().machine.lower()
+if 'aarch' in bt:
+  os.system('chmod 777 data;./data')
+else:
+  exit(' Sorry Device Not Support ')
